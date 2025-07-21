f0ff8ade6ad0e604c6c329f728fd95a6acd7efbb
SPking1112 <rajasriveelanbot.com>
2025-07-21 10:50:51 +0530

    emailjs completed testing pending

--- function.js
+++ function.js
@@ -21,6 +21,8 @@ function closemenu() {
   sidemenu.style.right = "-200px";
 }
 
+// Header Name
+
 function randomName() {
   const names = ["SP King", "Full Stack Web Developer", "Raja Sri Veelan"];
   const randomIndex = Math.floor(Math.random() * names.length);
@@ -28,4 +30,29 @@ function randomName() {
   document.getElementById("random-name").textContent = randomName;
 }
 
-setInterval(randomName, 2000);
+setInterval(randomName, 1000);
+
+console.log(document.getElementById("name").value);
+
+// Email js
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_5fafwys";
+  const templateID = "template_hdj8vxf";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      (document.getElementById("name").value = ""),
+        (document.getElementById("email").value = ""),
+        (document.getElementById("message").value = "");
+      console.log(res);
+      alert("Your Message sent Successfully");
+    })
+    .catch((err) => console.log(err));
+}
